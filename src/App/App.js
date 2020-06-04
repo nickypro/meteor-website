ccac3fb5cfb28b5d2d531dfeac51c275c3d8ac83
Nicky Pochinkov <pochinkn@tcd.ie>
2020-06-04 22:22:25 +0100

    Added fetching image urls

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios';
 
 const App = () => {
+  const [images, setImages] = React.useState([])
+
+  const fetchImages = async () => {
+    const when  = new Date().toISOString() 
+    const response  = await axios.get(`${window.location.origin}/api/images-by-date?when=${when}`)
+    if ( !(response.status==200) ){
+      return console.log("ERROR: could not fetch image info: \n", response)
+    }
+    setImages(response.data)
+  }
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
   return (
     <div>
       App Content
