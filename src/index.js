176c5ada84838061983ebd65f4aa315865150687
Nicky Pochinkov <pochinkn@tcd.ie>
2020-06-05 02:04:29 +0100

    Changed MUI theme to dark mode

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App'
 import 'regenerator-runtime/runtime'
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
 
 ReactDOM.render(
+  <ThemeProvider theme={theme}>
     <App/>
+  </ThemeProvider>
   ,
   document.getElementById('root')
 );
