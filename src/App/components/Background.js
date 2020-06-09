3d10454ff36b50467b0d379268e89e105db293c0
Nicky Pochinkov <pochinkn@tcd.ie>
2020-06-09 18:12:00 +0100

    added particlesjs to background

---
+++ src/App/components/Background.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Particles from 'react-particles-js'
+import config from '../assets/config/particlesjs-config.json'
+
+function Background(props) {
+  return <div className="background"> 
+  <Particles params={config}/>
+  </div>
+}
+
+export default Background
