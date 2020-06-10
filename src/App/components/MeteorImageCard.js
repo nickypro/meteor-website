638f69b43fa4f316226092daf9bdaf7c2cfe86e5
Nicky Pochinkov <pochinkn@tcd.ie>
2020-06-10 10:43:24 +0100

    Moved MeteorImageCard to be separate

---
+++ src/App/components/MeteorImageCard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import dateFormat from 'dateformat'
+
+const MeteorImageCard = (props) => {
+  const path = `${window.location.origin}/images${props.data.filePath}` 
+  return (
+  <div>
+  <div style={{display: "flex", flexDirection: "column", margin: "1rem"}}>
+    
+    <div style={{fontSize: "1.5rem", display: "flex", justifyContent: "space-between"}}>
+      <span style={{textAlign: "left" }}> {dateFormat(props.data.date, "d mmm yyyy", true)} </span>
+      <span style={{textAlign: "right"}}> {dateFormat(props.data.date, "hh:MM:ss", true)} </span>
+    </div>
+
+    <Card style={cardStyling}>
+    <a href={path}>
+      <img className="meteor-image" src={path}/>
+    </a>
+    <button 
+      id={`button_${props.data.filePath}`}
+      class={`slick-star ${props.starred ? "starred" : ""}`} 
+      onClick={props.toggleStar}
+      for="favourite meteor"
+      > 
+      &#9733;
+    </button>
+    <div style={textMarginStyling}>
+      <h2 style={{textAlign: "center"}}>Meteor Image</h2>
+      <ul>
+        <li> Cam  : {props.data.camera} </li>
+        <li> info : meteor     </li>
+      </ul>
+    </div>
+    <br/>
+    </Card>
+
+  </div>
+  </div>
+  )
+}
+
+const textMarginStyling = {width: "80%", margin: "0px auto"}
+
+const cardStyling = {
+  position: "relative",
+  maxWidth: "80vw",
+  background: "rgb(34, 54, 76)",
+}
+
+export default MeteorImageCard
--- src/App/components/MeteorImagesCarousel.js
+++ src/App/components/MeteorImagesCarousel.js
@@ -3,8 +3,7 @@ import Card from '@material-ui/core/Card'
 
 import Carousel from "react-slick"; 
 
-import dateFormat from 'dateformat'
-
+import MeteorImageCard from './MeteorImageCard';
 
 const ImageCarousel = (props) => {
   const carouselOptions = {
@@ -35,43 +34,14 @@ const ImageCarousel = (props) => {
     </div>
 
     {/* List meteor images */}
-    {props.images.map((item, index) => {
-      const path = `${window.location.origin}/images${item.filePath}` 
-      return (
-      <div>
-      <div style={{display: "flex", flexDirection: "column", margin: "1rem"}}>
-        
-        <div style={{fontSize: "1.5rem", display: "flex", justifyContent: "space-between"}}>
-          <span style={{textAlign: "left" }}> {dateFormat(item.date, "d mmm yyyy", true)} </span>
-          <span style={{textAlign: "right"}}> {dateFormat(item.date, "hh:MM:ss", true)} </span>
-        </div>
-
-        <Card style={cardStyling}>
-        <a href={path}>
-          <img className="meteor-image" key={index} src={path}/>
-        </a>
-        <button 
-          id={`button_${item.filePath}`}
-          class={`slick-star ${props.starred.has(item.filePath) ? "starred" : ""}`} 
-          onClick={() => props.toggleStar(item.filePath)}
-          for="favourite meteor"
-          > 
-          &#9733;
-        </button>
-        <div style={textMarginStyling}>
-          <h2 style={{textAlign: "center"}}>Meteor Image</h2>
-          <ul>
-            <li> Cam  : {item.camera} </li>
-            <li> info : meteor     </li>
-          </ul>
-        </div>
-        <br/>
-        </Card>
-
-      </div>
-      </div>
-      )}
-    )}
+    {props.images.map((item, index) => (
+      <MeteorImageCard 
+        key={`meteor_card_${index}`}
+        data={item} 
+        starred={props.starred.has(item.filePath)}
+        toggleStar={() => props.toggleStar(item.filePath)}
+      />
+    ))}
     {/* End of Meteor Image List */}
     <div>
     <div className="center-flex">
@@ -84,8 +54,6 @@ const ImageCarousel = (props) => {
     </div>
   </Carousel>
 )};
-
-const textMarginStyling = {width: "80%", margin: "0px auto"}
 
 const cardStyling = {
   position: "relative",
